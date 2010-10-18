463674d034baa4146db28e02d41e0c7b5629891c
Csaba Nemeth <cnemeth9@gmail.com>
2010-10-17 18:10:38 -0700

    added helper methods for user show

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,5 @@
 module UsersHelper
 
-  def gender
-    [['Male', true],['Female', false]]
-  end
-
   def roles
     Role.all.collect {|role|[role.name, role.id]}
   end
@@ -14,6 +10,33 @@ module UsersHelper
 
   def race_categories
     RaceCategory.all.collect  {|race_category|[race_category.name, race_category.id]}
+  end
+
+  def member_roles(ids)
+    role_ids = Role.find_all_by_id(ids)
+    roles = ""
+    role_ids.each do |r|
+      roles += r.name + ", "
+    end
+    roles
+  end
+
+  def member_race_disciplines(ids)
+    race_discipline_ids = RaceDiscipline.find_all_by_id(ids)
+    race_disciplines = ""
+    race_discipline_ids.each do |r|
+      race_disciplines += r.name + ", "
+    end
+    race_disciplines
+  end
+
+  def member_race_categories(ids)
+    race_category_ids = RaceCategory.find_all_by_id(ids)
+    race_categories = ""
+    race_category_ids.each do |r|
+      race_categories += r.name + ", "
+    end
+    race_categories
   end
 
 end
