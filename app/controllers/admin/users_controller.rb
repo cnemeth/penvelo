e7557d571d30dbb2ef2a82452f5dc7a957559367
Csaba Nemeth <cnemeth9@gmail.com>
2010-10-22 14:12:35 -0700

    admins now have full CRUD capability on users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,7 @@
 class Admin::UsersController < ApplicationController
 
   # acl9 access control
-  before_filter :load_users, :only => [:index, :show, :edit, :update, :destroy]
+  before_filter :load_users, :only => [:index, :show, :new, :create, :edit, :update, :destroy]
   access_control do
     allow :site_admin
   end
@@ -30,10 +30,40 @@ class Admin::UsersController < ApplicationController
     end
   end
 
+  # GET /admin/users/new
+  # GET /admin/users/new.xml
+  def new
+    @user = User.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @user }
+    end
+  end
+
+  # POST /admin/users
+  # POST /admin/users.xml
+  def create
+    @user = User.new(params[:user])
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to(admin_users_url :notice => 'User profile was successfully created.') }
+        format.xml  { render :xml => @user, :status => :created, :location => @user }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # GET /users/1/edit
   def edit
     @user = User.find(params[:id])
   end
 
+  # PUT /users/1
+  # PUT /users/1.xml
   def update
     @user = User.find(params[:id])
     #raise @user.inspect
@@ -65,10 +95,6 @@ private
   def load_users
     @users = User.all
   end
-
-
-
-
 
 end
 
