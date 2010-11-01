710a2091a41e74b7a5a8e203de161c96193fc52e
Csaba Nemeth <cnemeth9@gmail.com>
2010-11-01 09:18:59 -0700

    common::news index and helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,7 @@ class UsersController < ApplicationController
 
   def show
     @user = @current_user
+
   end
 
   def edit
--- app/helpers/common/newses_helper.rb
+++ app/helpers/common/newses_helper.rb
@@ -1,2 +1,8 @@
 module Common::NewsesHelper
+
+  def news_content
+    news_content = Content.find(:all, :conditions => "content_type_id = #{ContentType.find(:first, :conditions => "name = 'news'").id}", :order => "created_at DESC")
+  end
+
 end
+
