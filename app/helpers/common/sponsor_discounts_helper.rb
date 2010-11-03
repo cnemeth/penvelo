e4d0f071f2cf86d62684b0be989f582a081ec6d3
Csaba Nemeth <cnemeth9@gmail.com>
2010-11-02 18:22:08 -0700

    changed the look of the common::sponsor_discounts index page

--- app/helpers/common/sponsor_discounts_helper.rb
+++ app/helpers/common/sponsor_discounts_helper.rb
@@ -1,8 +1,4 @@
 module Common::SponsorDiscountsHelper
 
-  def sponsor_discounts
-    SponsorDiscount.all
-  end
-
 end
 
--- app/helpers/common/sponsors_helper.rb
+++ app/helpers/common/sponsors_helper.rb
@@ -1,5 +1,9 @@
 module Common::SponsorsHelper
 
+  def sponsors
+    Sponsor.all
+  end
+
   def sponsor_types
     SponsorType.all
   end
