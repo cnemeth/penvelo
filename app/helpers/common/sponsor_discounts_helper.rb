c99228553548906d8af417df2cf7025bfdabd71c
Csaba Nemeth <cnemeth9@gmail.com>
2010-11-02 18:02:31 -0700

    fixed error in common::sponsor_discounts index

--- app/helpers/common/sponsor_discounts_helper.rb
+++ app/helpers/common/sponsor_discounts_helper.rb
@@ -1,7 +1,7 @@
 module Common::SponsorDiscountsHelper
 
-  def sponsor_discounts(sponsor_name)
-    SponsorDiscount.find(:all, :conditions => "sponsor_id = #{Sponsor.find(:first, :conditions => "name = 'sponsor_name'").id}")
+  def sponsor_discounts
+    SponsorDiscount.all
   end
 
 end
