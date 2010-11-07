15e8efa6796caa3c7a39a0d570454e5ca03c10ff
Csaba Nemeth <cnemeth9@gmail.com>
2010-11-07 15:52:31 -0800

    about club index

--- app/controllers/common/clubs_controller.rb
+++ app/controllers/common/clubs_controller.rb
@@ -1,5 +1,16 @@
 class Common::ClubsController < ApplicationController
+
   def index
+    @clubs = Club.all
+    @pv_club = @clubs[0]
+
+    @club_officers = nil
+
+    @board_members = nil
+
+    @club_directors = nil
+
   end
 
 end
+
