41c26127800d6a6c30083688a6227ed222c4e393
Csaba Nemeth <cnemeth9@gmail.com>
2010-11-07 13:43:04 -0800

    added helper methods to listing members

--- app/helpers/common/members_helper.rb
+++ app/helpers/common/members_helper.rb
@@ -1,2 +1,17 @@
 module Common::MembersHelper
+
+  def user_image(user)
+    user.photo_file_name == nil ?
+      image_tag("generic_user.png") : image_tag(user.photo.url(:medium))
+  end
+
+  def user_age(user)
+    age(user) unless user.date_of_birth == nil
+  end
+
+  def image_gallery(user)
+    link_to("Click here to visit #{user.first_name}'s image gallery.", user.image_gallery_url, :target => '_blank')
+  end
+
 end
+
