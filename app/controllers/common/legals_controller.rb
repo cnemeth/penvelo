049aed93cb8d11a0ed421113130e2e31d32c61fb
Csaba Nemeth <cnemeth9@gmail.com>
2010-11-07 15:02:09 -0800

    privacy, legal

--- app/controllers/common/legals_controller.rb
+++ app/controllers/common/legals_controller.rb
@@ -1,5 +1,10 @@
 class Common::LegalsController < ApplicationController
+
   def index
+    @clubs = Club.all
+
+    @legal = @clubs[0].legal
   end
 
 end
+
--- app/controllers/common/privacies_controller.rb
+++ app/controllers/common/privacies_controller.rb
@@ -1,5 +1,10 @@
 class Common::PrivaciesController < ApplicationController
+
   def index
+    @clubs = Club.all
+
+    @privacy = @clubs[0].privacy
   end
 
 end
+
