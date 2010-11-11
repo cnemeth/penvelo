8c9f7c81dd9fbfb177f3049cc474f6c36438af52
Csaba Nemeth <cnemeth9@gmail.com>
2010-11-11 14:12:28 -0800

    added helper method to fill in race result table in side bar

--- app/helpers/admin/race_results_helper.rb
+++ app/helpers/admin/race_results_helper.rb
@@ -1,2 +1,15 @@
 module Admin::RaceResultsHelper
+
+  def placements(place)
+    count = 0
+    placements = RaceResult.find(:all, :conditions => ["placement <= ?", place])
+    placements.each do |f|
+      if f.updated_at.year == Time.now.year
+        count = count + 1
+      end
+    end
+    count
+  end
+
 end
+
