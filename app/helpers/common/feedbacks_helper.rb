045d7c7bbf4926124f910cab368a32fd2218a16f
Csaba Nemeth <cnemeth9@gmail.com>
2010-11-12 13:34:17 -0800

    radio buttons to select feedback type to display

--- app/helpers/common/feedbacks_helper.rb
+++ app/helpers/common/feedbacks_helper.rb
@@ -1,2 +1,24 @@
 module Common::FeedbacksHelper
+
+  def feedbacks
+    Feedback.find(:all, :order => "created_at DESC")
+  end
+
+  #def feedback_by_type(feedback_type)
+  #  feedbacks = nil
+  #  if feedback_type == "all"
+  #    feedbacks = Feedback.find(:all, :order => "created_at DESC")
+  #  else
+  #    feedback_types = FeddbackType.all
+  #    feedback_types.each do |f|
+  #      if f.name == feedback_type
+  #        feedbacks = Feedback.find(:all,
+  #                                  :conditions => ["feedback_type_id = ?", f.id]
+  #                                  :order => "created_at DESC")
+  #  end
+  #  feedbacks
+  #end
+
+
 end
+
