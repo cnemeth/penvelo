e6b3bb464e9b8a6d3663dcdc4a525cee03c0aeec
Csaba Nemeth <cnemeth9@gmail.com>
2010-11-17 18:34:47 -0800

    added will_paginate gem

--- config/environment.rb
+++ config/environment.rb
@@ -24,6 +24,7 @@ Rails::Initializer.run do |config|
   config.gem "formtastic"
   config.gem "annotate"
   config.gem "validation_reflection"
+  config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
