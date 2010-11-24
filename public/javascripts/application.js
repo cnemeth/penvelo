994ecf5c1df3ce1a0420cf69a88b29bb3d6c3c26
Csaba Nemeth <cnemeth9@gmail.com>
2010-11-24 11:39:56 -0800

    set jw player to continuous and repeat mode

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+<script type='text/javascript'>
+  var so = new SWFObject('player.swf','ply','800','240','9','#');
+  so.addParam('allowfullscreen','true');
+  so.addParam('allowscriptaccess','always');
+  so.addVariable('file','http://www.longtailvideo.com/jw/upload/tutorial-mrss.xml');
+  so.addVariable('playlistsize', '400');
+  so.addVariable('playlist','right');
+  so.write('mediaspace');
+</script>
+
